9409923a9dcaca3d6367f97dcf6361f1e5bb5915
s1byl <asdfghjklll123@gmail.com>
2022-09-07 17:38:10 +0800

    add db connection

--- db/database.py
+++ db/database.py
@@ -1,8 +1,17 @@
 from sqlalchemy import create_engine
 from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
+import urllib
 
-SQLALCHEMY_DATABASE_URL = "sqlite:///./fastapi-test.db"
+
+server = 'LAPTOP-27FOR7RN\SQLEXPRESS' # to specify an alternate port
+database = 'SCADA' 
+username = 'test' 
+password = '53709398'
+
+params = urllib.parse.quote_plus("'DRIVER={ODBC Driver 17 for SQL Server};SERVER='+server+';DATABASE='+database+';UID='+username+';PWD='+ password")
+
+SQLALCHEMY_DATABASE_URL = f"mssql+pyodbc:///?odbc_connect={params}"
 
 engine = create_engine(
     SQLALCHEMY_DATABASE_URL, connect_args={"check_same_thread": False}
